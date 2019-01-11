52ca823f78b48b158a78b390789ad09fb4f50024
guodong <guodongsoft@163.com>
2019-01-11 21:04:19 +0900

    evil

--- custom-post.el
+++ custom-post.el
@@ -4,16 +4,16 @@
 ;;;       Put your own configurations in custom-post.el to override default configurations.
 ;;; Code:
 
-;; Evil
-(use-package evil :ensure t)
-(evil-mode t)
-
 ;; Treemacs
 (with-eval-after-load 'treemacs
   (treemacs-reset-icons) ; Treemacs custom icons
   (evil-define-key 'normal treemacs-mode-map (kbd "o") 'treemacs-RET-action)
   (evil-define-key 'normal treemacs-mode-map (kbd "F") 'treemacs-create-file)
   (evil-define-key 'normal treemacs-mode-map (kbd "+") 'treemacs-create-dir))
+
+;; Evil
+(use-package evil :ensure t)
+(evil-mode t)
 
 (use-package evil-magit
   :ensure t
