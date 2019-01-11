7d058622bbfc02a128e688b8aded46eec09b9c8c
dong guo <dong.guo@mobilus.co.jp>
2019-01-11 14:19:18 +0900

    add treemacs keymap

--- custom-post.el
+++ custom-post.el
@@ -8,8 +8,11 @@
 (use-package evil :ensure t)
 (evil-mode t)
 
-;; Treemacs custom icons
+;; Treemacs
 (with-eval-after-load 'treemacs
- (treemacs-reset-icons))
+ (treemacs-reset-icons) ; Treemacs custom icons
+ (evil-define-key 'normal treemacs-mode-map (kbd "o") 'treemacs-RET-action)
+ (evil-define-key 'normal treemacs-mode-map (kbd "F") 'treemacs-create-file)
+ (evil-define-key 'normal treemacs-mode-map (kbd "+") 'treemacs-create-dir))
 
 ;;; custom-post.el ends here
